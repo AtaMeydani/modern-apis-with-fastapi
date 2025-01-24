3fd82135453fede59690a3e01183a2f59d8001fe
Ata Meydani <miratameydani@gmail.com>
2025-01-24 13:11:20 +0330

    Add requirements & Format Code

--- 02_language-foundations/01_types/no_types_program.py
+++ 02_language-foundations/01_types/no_types_program.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 
-Item = namedtuple('Item', 'name, value')
+Item = namedtuple("Item", "name, value")
 
 running_max = None
 
@@ -21,17 +21,23 @@ def counter(items):
 def main():
     print("Let's create some items")
 
-    dinner_items = [Item('Pizza', 20), Item('Beer', 9), Item('Beer', 9)]
-    breakfast_items = [Item('Pancakes', 11), Item('Bacon', 4), Item('Coffee', 3), Item('Coffee', 3), Item('Scone', 2)]
+    dinner_items = [Item("Pizza", 20), Item("Beer", 9), Item("Beer", 9)]
+    breakfast_items = [
+        Item("Pancakes", 11),
+        Item("Bacon", 4),
+        Item("Coffee", 3),
+        Item("Coffee", 3),
+        Item("Scone", 2),
+    ]
 
     dinner_total = counter(dinner_items)
-    print(f'Dinner was ${dinner_total:,.02f}')
+    print(f"Dinner was ${dinner_total:,.02f}")
 
     breakfast_total = counter(breakfast_items)
-    print(f'Breakfast was ${breakfast_total:,.02f}')
+    print(f"Breakfast was ${breakfast_total:,.02f}")
 
-    print(f'Today your most expensive meal costs ${running_max:.02f}')
+    print(f"Today your most expensive meal costs ${running_max:.02f}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
