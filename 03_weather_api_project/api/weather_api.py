f8fa74d65b511beff18abce2f0933fb0507f1ccd
Ata Meydani <miratameydani@gmail.com>
2025-01-26 14:47:55 +0330

    Binding pydantic models to route params

--- 03_weather_api_project/api/weather_api.py
+++ 03_weather_api_project/api/weather_api.py
@@ -1,14 +1,14 @@
 from typing import Optional
+from fastapi import Depends
 import fastapi
+
+from models.location import Location
+from services import openweather_service
 
 router = fastapi.APIRouter()
 
 
-@router.get("/api/weather/{}")
-def weather(
-    city: str,
-    state: Optional[str] = None,
-    country: str = "US",
-    units: Optional[str] = "metric",
-):
-    return f"{city}, {state}, {country} in {units}"
+@router.get("/api/weather/{city}")
+def weather(loc: Location = Depends(), units: Optional[str] = "metric"):
+    report = openweather_service.get_report(loc.city, loc.state, loc.country, units)
+    return report
--- 03_weather_api_project/main.py
+++ 03_weather_api_project/main.py
@@ -3,6 +3,9 @@ import uvicorn
 from starlette.staticfiles import StaticFiles
 from api import weather_api
 from views import home
+from services import openweather_service
+import json
+from pathlib import Path
 
 
 api = fastapi.FastAPI()
@@ -16,6 +19,21 @@ def configure_routing():
 
 def configure():
     configure_routing()
+    configure_api_keys()
+
+
+def configure_api_keys():
+    file = Path("settings.json").absolute()
+    if not file.exists():
+        print(
+            f"WARNING: {file} file not found, you cannot continue, please see settings_template.json"
+        )
+        raise Exception(
+            "settings.json file not found, you cannot continue, please see settings_template.json"
+        )
+    with open("settings.json") as fin:
+        settings = json.load(fin)
+        openweather_service.api_key = settings.get("api_key")
 
 
 if __name__ == "__main__":
