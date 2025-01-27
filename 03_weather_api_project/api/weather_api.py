db270702b7184065492fe93e07f4388d729c6c54
Ata Meydani <miratameydani@gmail.com>
2025-01-27 13:32:21 +0330

    Fix caching logic to handle wrong path error responses

--- 03_weather_api_project/api/weather_api.py
+++ 03_weather_api_project/api/weather_api.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from fastapi import Depends
 import fastapi
+from models.validation_error import ValidationError
 
 from models.location import Location
 from services import openweather_service
@@ -10,7 +11,9 @@ router = fastapi.APIRouter()
 
 @router.get("/api/weather/{city}")
 async def weather(loc: Location = Depends(), units: Optional[str] = "metric"):
-    report = await openweather_service.get_report_async(
-        loc.city, loc.state, loc.country, units
-    )
-    return report
+    try:
+        return await openweather_service.get_report_async(
+            loc.city, loc.state, loc.country, units
+        )
+    except ValidationError as ve:
+        return fastapi.Response(content=ve.error_msg, status_code=ve.status_code)
