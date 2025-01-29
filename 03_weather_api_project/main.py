728738f0287ac4a8b869039bdcb799449f6e2272
Ata Meydani <miratameydani@gmail.com>
2025-01-29 10:07:56 +0330

    Display recent weather events on the home page

--- 03_weather_api_project/main.py
+++ 03_weather_api_project/main.py
@@ -3,9 +3,11 @@ import uvicorn
 from starlette.staticfiles import StaticFiles
 from api import weather_api
 from views import home
-from services import openweather_service
+from services import openweather_service, report_service
+from models.location import Location
 import json
 from pathlib import Path
+import asyncio
 
 
 api = fastapi.FastAPI()
@@ -20,6 +22,7 @@ def configure_routing():
 def configure():
     configure_routing()
     configure_api_keys()
+    configure_fake_data()
 
 
 def configure_api_keys():
@@ -34,6 +37,13 @@ def configure_api_keys():
     with open("settings.json") as fin:
         settings = json.load(fin)
         openweather_service.api_key = settings.get("api_key")
+
+
+def configure_fake_data():
+    # This was added to make it easier to test the weather event reporting
+    loc = Location(city="Portland", state="OR", country="US")
+    asyncio.run(report_service.add_report("Misty sunrise today, beautiful!", loc))
+    asyncio.run(report_service.add_report("Clouds over downtown.", loc))
 
 
 if __name__ == "__main__":
